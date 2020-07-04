f2a01128ff00a24504915941ed2512c19537de30
Shakthi Sachintha <shakthisachintha@gmail.com>
2020-07-04 15:27:27 +0530

    bug fixes

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
 const express = require("express");
 const app = express();
 
-const express = require("express");
-const app = express();
 const winston = require("winston");
 
 require("./startup/logger")();
 require("./startup/routes")(app);
 require("./startup/mongo")();
 require("./startup/config")();
-
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  winston.info(`Listning on port ${port}`);
-});
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
