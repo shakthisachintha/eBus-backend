07a7c469e616b30184056ae7134a25de0e49bc0d
tharushiSamara <tharushisamara157@gmail.com>
2020-07-15 23:11:00 +0530

    Backend intergration for Buses

--- routes/api/bus.js
+++ routes/api/bus.js
@@ -4,7 +4,7 @@ const Joi = require("joi");
 const _ = require("lodash");
 const mongoose = require('mongoose');
 const auth = require("../../middleware/auth");
-const { schema } = require("../../models/bus");
+
 
 const router = express.Router();
 
@@ -12,59 +12,81 @@ function validateBus(bus) {
     const schema = {
         busNo: Joi.string().min(6).required().label("Bus Number"),
         busRoute: Joi.string().required().label("Bus Route"),
-        busCapacity: Joi.number().max(2).required().label("Bus Capacity")
+        busCapacity: Joi.number().max(50).required().label("Bus Capacity")
     };
     return Joi.validate(bus, schema);
 }
 
-router.route('/').get(function(req, res) {
-    Bus.find(function(err, eBus) {
-        if (err) {
-            console.log(err);
-        }else {
-            res.json(ebus);
-        }    
-    });
+// router.route('/view').get(function (req, res) {
+//     // Bus.find(function (err, eBus) {
+//     //     if (err) {
+//     //         console.log(err);
+//     //     } else {
+//     //         res.json(ebus);
+//     //     }
+//     // });
+//     buses = await Bus.find().pretty();
+//     res.send(buses);
+// });
+
+router.get("/", async (req, res) => {
+    try {
+      const bus = await Bus.find();
+      res.status(200).send(bus);
+    } catch (error) {
+      res.status(400).send(error.message);
+    }
+  });
+
+//   router.route('/:id').get(function (req, res) {
+//     let id = req.params.id;
+//     Bus.findById(id, function (err, busDetail) {
+//         res.json(busDetail)
+//     });
+// });
+
+router.get("/:id", async (req, res) => {
+    try {
+    const bus = await Bus.findById(req.bus.id);
+    res.status(200).send(bus);
+    } catch (error) {
+    res.status(400).send(error.message);
+    }
 });
 
-router.route('/:id').get(function(req,res){
-    let id = req.params.id;
-    Bus.findById(id, function(err, busDetail) {
-        res.json(busDetail)
-    });
-});
-
-router.route('/register').post(function(req,res) {
+// router.route('/register').post(function (req, res) {
+router.post("/register", async (req, res) => {
     const { error } = validateBus(req.body);
     if (error) return res.status(400).send({ error: error.details[0].message });
 
     let buses = new Bus(req.body);
     buses.save()
         .then(buses => {
-            res.status(200).json({'buses': 'buses added successfully'});
+            res.status(200).json({ 'buses': 'buses added successfully' });
         })
         .catch(err => {
             res.status(400).send('adding new buses failed');
         });
 });
 
-router.route('/update/:id').post(function(req, res) {
-    Bus.findById(req.params.id, function(err, busDetail) {
+// router.route('/update/:id').post(function (req, res) {
+router.post("/update/:id", async (req, res) => {
+    Bus.findById(req.params.id, function (err, busDetail) {
         if (!busDetail) {
             res.status(404).send('data is not found');
 
-        } else {
+        } else 
             busDetail.busNo = req.body.busNo;
             busDetail.busRoute = req.body.busRoute;
             busDetail.busCapacity = req.body.busCapacity;
-        
+
             busDetail.save().then(busDetail => {
                 res.json('Bus Details updated');
             })
-            .catch(err => {
-                res.status(400).send("Update not possible");
-            });
-        }     
+                .catch(err => {
+                    res.status(400).send("Update not possible");
+                });
+        
     });
 });
 
--- startup/routes.js
+++ startup/routes.js
@@ -8,6 +8,12 @@ const error = require("../middleware/error");
 
 
 module.exports = function (app) {
+    app.use(function(req, res, next) {
+        res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        next();
+      });
+
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use("/api/user", userRoutes);
